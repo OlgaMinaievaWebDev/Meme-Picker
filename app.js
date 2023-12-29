f2a13c1acc7b492c346fe2567e3b5347efce861d
Olga <minaeva9@gmail.com>
2023-12-29 18:10:00 -0500

    modified

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 import { catsData } from "./data.js";
 
 const emotionRadios = document.getElementById("emotion-radios");
+const getImageBtn = document.getElementById("get-image-btn");
+const gifsOnlyOption = document.getElementById("gifs-only-option");
 
 //highlight emotion when picking it
 emotionRadios.addEventListener("change", highlightCheckedOption);
+
+getImageBtn.addEventListener("click", renderCat);
 
 function highlightCheckedOption(e) {
   //remove any highlight
@@ -11,10 +15,38 @@ function highlightCheckedOption(e) {
   for (const radio of radios) {
     radio.classList.remove("highlight");
   }
-
-  //add highlight onchange
+  // remove all instances of the highlight class
   document.getElementById(e.target.id).parentElement.classList.add("highlight");
 }
+
+function getMatchingCatsArray() {
+  if (document.querySelector('input[type="radio"]:checked')) {
+    const selectedEmotion = document.querySelector(
+      'input[type="radio"]:checked'
+    ).value;
+    const isGif = gifsOnlyOption.checked;
+
+    const matchingCatsArray = catsData.filter(function (cat) {
+      if (isGif) {
+        return cat.emotionTags.includes(selectedEmotion) && cat.isGif;
+      } else {
+      }
+      return cat.emotionTags.includes(selectedEmotion);
+    });
+    return matchingCatsArray;
+  }
+}
+
+function getSingleCatObject() {
+  const catsArray = getMatchingCatsArray();
+
+  if (catsArray.length === 1) {
+    return catsArray[0];
+  } else {
+  }
+}
+
+function renderCat() {}
 
 //get array of emotions form dataCats array
 function getEmotionArray(cats) {
